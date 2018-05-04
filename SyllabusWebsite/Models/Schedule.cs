2ff57caa9ace6cf7724281f89251eba14b538c1e
Michael Mills <michaelmills@quickenloans.com>
2018-05-04 16:23:59 -0400

    Added Schedules Controller

--- SyllabusWebsite/Models/Schedule.cs
+++ SyllabusWebsite/Models/Schedule.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace SyllabusWebsite.Models
 {
     public class Schedule
     {
+        [Key]
+        public int SyllabusId { get; set; }
+        public int Week { get; set; }
+        public string Topic { get; set; }
+        public string Description { get; set; }
     }
 }
